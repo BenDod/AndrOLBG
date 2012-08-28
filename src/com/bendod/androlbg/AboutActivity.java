5571a2e29dd2e76fa2296eee9c54163f2eea5ebd
BenDod <BenDod.dev@gmail.com>
2012-08-28 23:55:41 +0300

    First Release

--- src/com/bendod/androlbg/AboutActivity.java
+++ src/com/bendod/androlbg/AboutActivity.java
@@ -23,6 +23,14 @@ public class AboutActivity extends AbstractActivity {
      * @param view
      *            unused here but needed since this method is referenced from XML layout
      */
+    public void donate(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=UYP9SDCC3K5KG&lc=GB&item_name=AndrOLBG%20by%20BenDod&currency_code=GBP&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted")));
+    }
+
+    /**
+     * @param view
+     *            unused here but needed since this method is referenced from XML layout
+     */
     public void support(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:BenDod.Dev@gmail.com")));
     }
@@ -33,6 +41,14 @@ public class AboutActivity extends AbstractActivity {
      */
     public void twitter(View view) {
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.twitter.com/bendod_dev")));
+    }
+
+    /**
+     * @param view
+     *            unused here but needed since this method is referenced from XML layout
+     */
+    public void code(View view) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/BenDod/AndrOLBG")));
     }
 
 }
